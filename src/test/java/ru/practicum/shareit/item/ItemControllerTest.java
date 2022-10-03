182ea80aaf8b1557c5a6cd0dca4ce99a21fb7594
Dmitriy.Novikov <Dmitriy.Novikov@nexign.com>
2022-10-04 01:27:25 +0300

    ControllerTest

---
+++ src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.item;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemControllerTest {
+
+    @Test
+    void findUserItems() {
+    }
+
+    @Test
+    void createItem() {
+    }
+
+    @Test
+    void updateItem() {
+    }
+
+    @Test
+    void findItemById() {
+    }
+
+    @Test
+    void deleteItemById() {
+    }
+
+    @Test
+    void searchItems() {
+    }
+
+    @Test
+    void addItemComment() {
+    }
+}
---
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,147 @@
+package ru.practicum.shareit.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+    @MockBean
+    private UserService service;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    private User user = new User();
+
+    @BeforeEach
+    void setUp() {
+        user.setId(1L);
+        user.setName("Dima");
+        user.setEmail("dimano@mail.ru");
+    }
+
+    @Test
+    void createUser() throws Exception {
+        when(service.saveUser(any()))
+                .thenReturn(user);
+
+        mvc.perform(post("/users")
+                .content(mapper.writeValueAsString(user))
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(user.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(user.getName())))
+                .andExpect(jsonPath("$.email", is(user.getEmail())));
+    }
+
+    @Test
+    void getAllUsers() throws Exception {
+        when(service.getAllUsers())
+                .thenReturn(List.of(user));
+
+        mvc.perform(get("/users")
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(user.getId()), Long.class))
+                .andExpect(jsonPath("$[0].name", is(user.getName())))
+                .andExpect(jsonPath("$[0].email", is(user.getEmail())));
+    }
+
+    @Test
+    void updateUser() throws Exception {
+        when(service.updateUser(any()))
+                .thenReturn(Optional.of(user));
+
+        mvc.perform(patch("/users/{id}", 1)
+                .content(mapper.writeValueAsString(user))
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(user.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(user.getName())))
+                .andExpect(jsonPath("$.email", is(user.getEmail())));
+
+        when(service.updateUser(any()))
+                .thenReturn(Optional.empty());
+
+        mvc.perform(patch("/users/{id}", 1)
+                .content(mapper.writeValueAsString(user))
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void findUserById() throws Exception {
+        when(service.getUser(anyLong()))
+                .thenReturn(Optional.of(user));
+
+        mvc.perform(get("/users/{id}", 1)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(user.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(user.getName())))
+                .andExpect(jsonPath("$.email", is(user.getEmail())));
+
+        when(service.getUser(anyLong()))
+                .thenReturn(Optional.empty());
+
+        mvc.perform(get("/users/{id}", 2)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void deleteUserById() throws Exception {
+        when(service.deleteUser(anyLong()))
+                .thenReturn(true);
+
+        mvc.perform(delete("/users/{id}", 1)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        when(service.deleteUser(anyLong()))
+                .thenReturn(false);
+
+        mvc.perform(delete("/users/{id}", 2)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+}
