8831ffa8c46a71afa37547aed8fb788f6c3904b9
Dmitriy.Novikov <Dmitriy.Novikov@nexign.com>
2022-10-04 18:49:42 +0300

    RepositoryTest

---
+++ src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
@@ -0,0 +1,39 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Autowired
+    private UserRepository repository;
+
+    private User user = new User();
+
+    @BeforeEach
+    void setUp() {
+        user.setName("Dima");
+        user.setEmail("dimano@mail.ru");
+        em.persist(user);
+    }
+
+    @Test
+    void findByEmail() {
+        Optional<User> foundItem = repository.findByEmail("dimano@mail.ru");
+        assertThat(foundItem.isPresent());
+
+        foundItem = repository.findByEmail("dimano2@mail.ru");
+        assertThat(foundItem.isEmpty());
+    }
+}
